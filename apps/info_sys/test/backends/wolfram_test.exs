15a42c4a7a19c11935c12f288ce1e11ceb030563
Ed Gibbs <edward_gibbs@yahoo.com>
2016-02-18 19:19:05 -0800

    Add test to check wolfram process terminates

--- apps/info_sys/test/backends/wolfram_test.exs
+++ apps/info_sys/test/backends/wolfram_test.exs
@@ -5,7 +5,9 @@ defmodule InfoSys.Backends.WolframTest do
   test "makes request, reports results, then terminates" do
     ref = make_ref()
     {:ok, pid} = Wolfram.start_link("1+1", ref, self(), 1)
+    Process.monitor(pid)
 
     assert_receive {:results, ^ref, [%InfoSys.Result{text: "2"}]}
+    assert_receive {:DOWN, _ref, :process, ^pid, :normal}
   end
 end
